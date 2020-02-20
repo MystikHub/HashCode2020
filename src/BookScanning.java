58caf7adda351383e04c7d3ad71f7d0903ad4914
Abhay Khanka <58368777+abhayKRak3n@users.noreply.github.com>
2020-02-20 20:21:39 +0000

    Update BookScanning.java

--- src/BookScanning.java
+++ src/BookScanning.java
@@ -1,13 +1,18 @@
+package googleHashCode;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BookScanning {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// Open the file
-		File dataset = new File("f_libraries_of_the_world.txt");
+		File dataset = new File("a_example.txt");
 		Scanner fileReader;
 		try {
 			fileReader = new Scanner(dataset);
@@ -35,20 +40,21 @@ public class BookScanning {
 			allBooks.add(newBook);
 		}
 		
-		// Read the library data into our java classes
+		// System.out.println("Books: " + allBooks.toString());
+		
 		Scanner libraryInfoScanner, libraryBookScanner;
 		ArrayList<Library> allLibraries = new ArrayList<Library>();
 		for(int i = 0; i < libraryCount; i++) {
 			// Populate the libraries here
 			libraryInfoScanner = new Scanner(fileReader.nextLine());
 			libraryBookScanner = new Scanner(fileReader.nextLine());
-
+			
 			int libraryBookCount = libraryInfoScanner.nextInt();
 			int librarySignupDuration = libraryInfoScanner.nextInt();
 			int libraryBooksShippablePerDay = libraryInfoScanner.nextInt();
 			
 			Library newLibrary = new Library(libraryBookCount, librarySignupDuration, libraryBooksShippablePerDay);
-			
+			newLibrary.id = i;
 			ArrayList<Book> libraryBooks = new ArrayList<Book>();
 			for(int j = 0; j < libraryBookCount; j++) {
 				int bookId = libraryBookScanner.nextInt();
@@ -60,9 +66,43 @@ public class BookScanning {
 		}
 		
 		// Problem logic
+		writeFile(allLibraries);
 		
+	
 		
 		bookScanner.close();
 		fileReader.close();
+	}
+	
+	
+	public static void writeFile(ArrayList<Library> allLibraries) throws IOException {
+		File output = new File("results.txt");
+		FileOutputStream fos = new FileOutputStream(output);
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+		
+		Library.sortLibraries(allLibraries);
+		bw.write(allLibraries.size());// Writes Number of Library
+		System.out.println(allLibraries.size()+"\n");
+		bw.newLine();
+		int i = 0;
+		
+		while(i<allLibraries.size()) {
+			bw.write(allLibraries.get(i).id+" "+allLibraries.get(i).timeForSignUp);
+			System.out.println((allLibraries.get(i).id+" "+allLibraries.get(i).timeForSignUp));
+			bw.newLine();
+			int j=0;
+			while(j<allLibraries.get(i).numberOfBooks) {
+			
+				bw.write(allLibraries.get(i).Library.get(j).bookId + " ");
+				System.out.print(allLibraries.get(i).Library.get(j).bookId + " ");
+				
+				j++;	
+			}
+			bw.newLine();
+			System.out.println("\n");
+			
+		i++;	
+		}
 	}
 }
