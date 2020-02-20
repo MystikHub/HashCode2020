3315802124351108cd05de17647fc9e100deb06e
Abhay Khanka <58368777+abhayKRak3n@users.noreply.github.com>
2020-02-20 19:53:06 +0000

    Update Library.java

--- src/Library.java
+++ src/Library.java
@@ -4,11 +4,13 @@ import java.util.ArrayList;
 public class Library {
 	ArrayList<Book> Library = new ArrayList<Book>();
 	//int setupTime;
+	int id;
 	public int numberOfBooks;
 	public int timeForSignUp;		//time in days 
 	public int booksSent;		//books scanned/day
 	
 	public Library(int numOfBooks,int setupTime,int bookSent) {
+			
 			this.numberOfBooks = numOfBooks;
 			this.timeForSignUp = setupTime;
 			this.booksSent = bookSent;
