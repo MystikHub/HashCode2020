fdab1be1140419d91a2685a0067997891beec3ef
Mystik <shutanu@pm.me>
2020-02-20 18:37:51 +0000

    Added example file and main class

---
+++ src/BookScanning.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class BookScanning {
+
+	public static void main(String[] args) {
+		File dataset = new File("a_example.txt");
+		Scanner fileReader;
+		try {
+			fileReader = new Scanner(dataset);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
